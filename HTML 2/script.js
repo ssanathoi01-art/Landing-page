9f2bacde2d67421c09055b8bdc44e9772ca0d503
Salam Sanathoi Meitei <ssanathoi01@gmail.com>
2026-01-23 20:45:02 +0530

    first commit

---
+++ HTML 2/script.js
@@ -0,0 +1,14 @@
+
+function display2(g, h) {
+  return g / h;
+}
+
+document.getElementById("result").innerText = display2(10, 4);
+
+
+function show() {
+  let x = 20;
+  let y = 6;
+  document.getElementById("result").innerText = x / y;
+}
+ 
